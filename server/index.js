ad35d53bb818307761ea17f0b1201522c6853c1c
henryhbell <henryhamerbell@gmail.com>
2020-09-04 20:00:42 -0400

    Stress Testing Complete

--- server/index.js
+++ server/index.js
@@ -1,23 +1,25 @@
+const nr = require('newrelic');
+
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 const port = 3000;
 
-app.use('/:id', express.static('public'));
+// app.use('/:id', express.static('public'));
 
-// Photo Carousel
-app.use('/api/photos/:restaurantID', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
+// // Photo Carousel
+// app.use('/api/photos/:restaurantID', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
 
 // Info Sidebar
-app.use('/:copyId/restaurants/:id', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true }));
+app.use(createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true }));
 
-// Tips & Recommendations
-app.use('/api/tips/:id', createProxyMiddleware({ target: 'http://localhost:6070', changeOrigin: true }));
-app.use('/api/articles/:id', createProxyMiddleware({ target: 'http://localhost:6070', changeOrigin: true }));
+// // Tips & Recommendations
+// app.use('/api/tips/:id', createProxyMiddleware({ target: 'http://localhost:6070', changeOrigin: true }));
+// app.use('/api/articles/:id', createProxyMiddleware({ target: 'http://localhost:6070', changeOrigin: true }));
 
-// Similar Restaurants
-app.use('/restaurants/:id', createProxyMiddleware({ target: 'http://localhost:3004', changeOrigin: true }));
+// // Similar Restaurants
+// app.use('/restaurants/:id', createProxyMiddleware({ target: 'http://localhost:3004', changeOrigin: true }));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
